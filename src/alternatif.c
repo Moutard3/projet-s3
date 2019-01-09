20d41f45d08c9cbd38ae08cc30e604b18639c70c
Alexandre "Moutarde" S <moutardea@gmail.com>
2019-01-09 08:37:10 +0100

    Création fichier alternatif

---
+++ src/alternatif.c
@@ -0,0 +1,13 @@
+/**
+ * @file alternatif.c
+ * @brief 
+ * @author Alexandre Saillet
+ * @date CR 06/01/2019
+ * @date LU 06/01/2019
+ */
+
+#include "alternatif.h"
+
+void vainqueurAlternatif(t_mat_char_star_dyn matcsv, int *indVainqueur) {
+
+}
---
+++ src/alternatif.h
@@ -0,0 +1,16 @@
+/**
+ * @file alternatif.h
+ * @brief 
+ * @author Alexandre Saillet
+ * @date CR 06/01/2019
+ * @date LU 06/01/2019
+ */
+
+#ifndef PROJET_S3_ALTERNATIF_H
+#define PROJET_S3_ALTERNATIF_H
+
+#include "global.h"
+
+void vainqueurAlternatif(t_mat_char_star_dyn matcsv, int *indVainqueur);
+
+#endif //PROJET_S3_ALTERNATIF_H
